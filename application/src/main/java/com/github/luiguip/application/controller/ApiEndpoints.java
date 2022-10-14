f6afdecc403b1c129e07889af420aa77c40848d9
Luis Guilherme de A <luisguilhermepdea@gmail.com>
2022-10-13 21:53:59 -0300

    Created controller

---
+++ application/src/main/java/com/github/luiguip/application/controller/ApiEndpoints.java
@@ -0,0 +1,11 @@
+package com.github.luiguip.application.controller;
+
+import lombok.NoArgsConstructor;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@NoArgsConstructor(access = PRIVATE)
+public class ApiEndpoints {
+
+    public static final String UFS_ENDPOINT = "/ufs";
+}
---
+++ application/src/main/java/com/github/luiguip/application/controller/IbgeUfsController.java
@@ -0,0 +1,28 @@
+package com.github.luiguip.application.controller;
+
+import com.github.luiguip.application.mapper.IbgeUfDtoMapper;
+import com.github.luiguip.domain.port.application.IbgeUfsServicePort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static com.github.luiguip.application.controller.ApiEndpoints.UFS_ENDPOINT;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(UFS_ENDPOINT)
+public class IbgeUfsController {
+
+    private final IbgeUfsServicePort service;
+
+    private final IbgeUfDtoMapper mapper;
+
+    @GetMapping
+    List<IbgeUfDto> findAll() {
+        var ufs = service.findAll();
+        return mapper.toDto(ufs);
+    }
+}
