eb39849387168abd88fbabcf66480ef510b98808
Luis Guilherme de A <luisguilhermepdea@gmail.com>
2022-10-13 21:53:30 -0300

    refactored constructor with lombok

--- infrastructure/src/main/java/com/github/luiguip/infrastructure/client/IbgeEndpoints.java
+++ infrastructure/src/main/java/com/github/luiguip/infrastructure/client/IbgeEndpoints.java
@@ -1,10 +1,11 @@
 package com.github.luiguip.infrastructure.client;
 
+import lombok.NoArgsConstructor;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@NoArgsConstructor(access = PRIVATE)
 public class IbgeEndpoints {
-
-    private IbgeEndpoints() {
-
-    }
 
     public static final String UFS_ENDPOINT = "/v1/localidades/estados";
 }
