e53d1390527518ad64953c81d64bc05c3fc95f5b
Luis Guilherme de A <luisguilhermepdea@gmail.com>
2022-10-13 21:53:45 -0300

    Added dto

---
+++ application/src/main/java/com/github/luiguip/application/controller/IbgeUfDto.java
@@ -0,0 +1,4 @@
+package com.github.luiguip.application.controller;
+
+public record IbgeUfDto(Integer id, String name, String abbreviation) {
+}
---
+++ application/src/main/java/com/github/luiguip/application/mapper/IbgeUfDtoMapper.java
@@ -0,0 +1,14 @@
+package com.github.luiguip.application.mapper;
+
+import com.github.luiguip.application.controller.IbgeUfDto;
+import com.github.luiguip.domain.model.IbgeUf;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
+
+@Mapper(componentModel = SPRING)
+public interface IbgeUfDtoMapper {
+    List<IbgeUfDto> toDto(List<IbgeUf> ufs);
+}
