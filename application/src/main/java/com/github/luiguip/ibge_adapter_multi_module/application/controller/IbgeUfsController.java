e9d0ff1dc624217edad86a418c560906490d4d8d
Luis Guilherme de A <luisguilhermepdea@gmail.com>
2022-10-19 18:43:16 -0300

    Added produces to request mapping

--- application/src/main/java/com/github/luiguip/ibge_adapter_multi_module/application/controller/IbgeUfsController.java
+++ application/src/main/java/com/github/luiguip/ibge_adapter_multi_module/application/controller/IbgeUfsController.java
@@ -11,10 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 import static com.github.luiguip.ibge_adapter_multi_module.application.controller.ApiEndpoints.UFS_ENDPOINT;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping(UFS_ENDPOINT)
+@RequestMapping(value = UFS_ENDPOINT,
+        produces = APPLICATION_JSON_VALUE)
 public class IbgeUfsController {
 
     private final IbgeUfsServicePort service;
