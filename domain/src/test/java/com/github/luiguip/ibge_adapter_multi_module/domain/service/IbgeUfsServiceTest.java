f60598f30580bb4011c08987c8b327371cfe86ea
Luis Guilherme de A <luisguilhermepdea@gmail.com>
2022-10-20 17:38:15 -0300

    Added test cases to service

--- domain/src/test/java/com/github/luiguip/ibge_adapter_multi_module/domain/service/IbgeUfsServiceTest.java
+++ domain/src/test/java/com/github/luiguip/ibge_adapter_multi_module/domain/service/IbgeUfsServiceTest.java
@@ -1,5 +1,8 @@
 package com.github.luiguip.ibge_adapter_multi_module.domain.service;
 
+import com.github.luiguip.ibge_adapter_multi_module.domain.exception.PersistenceClientException;
+import com.github.luiguip.ibge_adapter_multi_module.domain.exception.PersistenceServerException;
+import com.github.luiguip.ibge_adapter_multi_module.domain.model.IbgeUf;
 import com.github.luiguip.ibge_adapter_multi_module.domain.port.infrastructure.IbgeUfsPersistencePort;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -8,9 +11,12 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Collections;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
 
 @ExtendWith(MockitoExtension.class)
 class IbgeUfsServiceTest {
@@ -31,5 +37,51 @@ class IbgeUfsServiceTest {
 
         //then
         assertThat(actual).isEmpty();
+    }
+
+    @Test
+    void shouldReturnListWhenItsReceived() {
+        //given
+        var expected = fixtureIbgeUfs();
+
+        //when
+        doReturn(expected)
+                .when(infra)
+                .findAll();
+        var actual = service.findAll();
+
+        //then
+        assertThat(expected)
+                .isEqualTo(actual);
+    }
+
+    @Test
+    void shouldThrowPersistenceClientExceptionWhenItsThrownByClient() {
+        //given, when
+        doThrow(PersistenceClientException.class)
+                .when(infra)
+                .findAll();
+
+        //then
+        assertThatThrownBy(() -> service.findAll())
+                .isInstanceOf(PersistenceClientException.class);
+    }
+
+    @Test
+    void shouldThrowPersistenceServerExceptionWhenItsThrownByClient() {
+        //given, when
+        doThrow(PersistenceServerException.class)
+                .when(infra)
+                .findAll();
+
+        //then
+        assertThatThrownBy(() -> service.findAll())
+                .isInstanceOf(PersistenceServerException.class);
+    }
+
+    private List<IbgeUf> fixtureIbgeUfs() {
+        return List.of(
+                new IbgeUf(1, "São Paulo", "SP"),
+                new IbgeUf(2, "Rio de Janeiro", "RJ"));
     }
 }
