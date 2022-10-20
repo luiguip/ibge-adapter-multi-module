2103be1d4a615fae30e4ebd342d04841151abaf8
Luis Guilherme de A <luisguilhermepdea@gmail.com>
2022-10-20 17:30:13 -0300

    Added test cases do persistence adapter

--- infrastructure/src/test/java/com/github/luiguip/ibge_adapter_multi_module/infrastructure/adapter/IbgeUfsPersistenceAdapterTest.java
+++ infrastructure/src/test/java/com/github/luiguip/ibge_adapter_multi_module/infrastructure/adapter/IbgeUfsPersistenceAdapterTest.java
@@ -1,19 +1,26 @@
 package com.github.luiguip.ibge_adapter_multi_module.infrastructure.adapter;
 
-import com.github.luiguip.ibge_adapter_multi_module.infrastructure.mapper.IbgeUfMapper;
-import com.github.luiguip.ibge_adapter_multi_module.infrastructure.client.IbgeUfsClient;
+import com.github.luiguip.ibge_adapter_multi_module.domain.exception.PersistenceClientException;
+import com.github.luiguip.ibge_adapter_multi_module.domain.exception.PersistenceServerException;
 import com.github.luiguip.ibge_adapter_multi_module.domain.model.IbgeUf;
-import org.assertj.core.api.Assertions;
+import com.github.luiguip.ibge_adapter_multi_module.infrastructure.client.IbgeUfsClient;
+import com.github.luiguip.ibge_adapter_multi_module.infrastructure.client.response.IbgeUfResponse;
+import com.github.luiguip.ibge_adapter_multi_module.infrastructure.mapper.IbgeUfMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mapstruct.factory.Mappers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Collections;
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
 
 @ExtendWith(MockitoExtension.class)
 class IbgeUfsPersistenceAdapterTest {
@@ -25,24 +32,74 @@ class IbgeUfsPersistenceAdapterTest {
     private IbgeUfsClient client;
 
     @Spy
-    IbgeUfMapper ibgeUfMapper;
+    IbgeUfMapper ibgeUfMapper = Mappers.getMapper(IbgeUfMapper.class);
 
     @Test
     void shouldReturnEmptyListWhenItsReceived() {
         //given
+        var response = Collections.emptyList();
 
         //when
-        Mockito.doReturn(fixture())
+        doReturn(response)
                 .when(client)
                 .findAll();
         var ufs = adapter.findAll();
 
         //then
-        Assertions.assertThat(ufs)
+        assertThat(ufs)
                 .isEmpty();
     }
 
-    private List<IbgeUf> fixture() {
-        return Collections.emptyList();
+    @Test
+    void shouldReturnListWhenItsReceived() {
+        //given
+        var response = fixtureIbgeUfsResponse();
+        var expected = fixtureIbgeUfs();
+
+        //when
+        doReturn(response)
+                .when(client)
+                .findAll();
+        var actual = adapter.findAll();
+
+        //then
+        assertThat(expected)
+                .isEqualTo(actual);
+    }
+
+    @Test
+    void shouldThrowPersistenceClientExceptionWhenItsThrownByClient() {
+        //given, when
+        doThrow(PersistenceClientException.class)
+                .when(client)
+                .findAll();
+
+        //then
+        assertThatThrownBy(() -> adapter.findAll())
+                .isInstanceOf(PersistenceClientException.class);
+    }
+
+    @Test
+    void shouldThrowPersistenceServerExceptionWhenItsThrownByClient() {
+        //given, when
+        doThrow(PersistenceServerException.class)
+                .when(client)
+                .findAll();
+
+        //then
+        assertThatThrownBy(() -> adapter.findAll())
+                .isInstanceOf(PersistenceServerException.class);
+    }
+
+    private List<IbgeUf> fixtureIbgeUfs() {
+        return List.of(
+                new IbgeUf(1, "São Paulo", "SP"),
+                new IbgeUf(2, "Rio de Janeiro", "RJ"));
+    }
+
+    private List<IbgeUfResponse> fixtureIbgeUfsResponse() {
+        return List.of(
+                new IbgeUfResponse(1, "São Paulo", "SP"),
+                new IbgeUfResponse(2, "Rio de Janeiro", "RJ"));
     }
 }
